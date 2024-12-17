80d107432d8b5e629db3121af93dbdddb60d5ddd
Thiago Henrique <thiaghenrique145@gmail.com>
2024-12-17 17:30:29 -0300

    Feat:

--- app/main.php
+++ app/main.php
@@ -6,6 +6,15 @@ include_once "model/Usuario.php";
 //instancia as classes
 $usuario = new Usuario();
 $usuariodao = new UsuarioDAO();
+
+$requisicao = isset($_GET['page']);
+
+switch($requisicao) {
+  case "ordensdeproducao": 
+      include "views/ordensdeproducao.php";
+      break;
+  default:
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -19,22 +28,13 @@ $usuariodao = new UsuarioDAO();
 <body>
 <nav class="navbar navbar-dark bg-dark justify-content-center">
 <div class="navbar">
-  <ul class="nav justify-content-center">
-    <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Home</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="">Gestão de Usuarios</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </li>
-  </ul>
+        <div class="container">
+            <h1 class="navbar-brand" href="#">
+                Gestão de Tarefas
+            </h1>
+        </div>
 </div>
-<button class="btn btn-light position-absolute top-20 start-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">Mais</button>
+<button class="btn btn-light position-absolute top-20 start-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">||||</button>
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
@@ -46,15 +46,14 @@ $usuariodao = new UsuarioDAO();
 </div>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">Offcanvas with backdrop</h5>
+    <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel"></h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body bg-dark">
-    <ul>
-      <li>
-      <a href="views/gestaoDeUsuarios.php" class="text-white bg-dark">Gestão de usuarios</a>
-      </li>
-    </ul>
+      <br>
+      <span class="d-block p-2 bg-dark text-white"><a href="views/gestaoDeUsuarios.php" class="text-white bg-dark">Gestão de usuarios</a></span>
+      <br>
+      <span class="d-block p-2 bg-dark text-white"><a href="?page=ordensdeproducao" class="text-white bg-dark">Ordens de Produção</a></span>
   </div>
 </div>
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
@@ -71,19 +70,7 @@ $usuariodao = new UsuarioDAO();
 <h1>Olá, <?php echo $_SESSION['nome_usuario'];?></h1>
 <br>
 
-
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php 
-$page = isset($_GET['page']);
-
-switch($page){
-case 'home': 
-    include 'main.php';
-    break;
-//case 'gestaousuarios': 
-    //include 'views/gestaoDeUsuarios.php';
-   //break;
-} ?> 
